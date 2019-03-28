d97623890826a0aee3340cc9ab6c309dd7f2cd4f
SPVJ <42379752+SPVJ@users.noreply.github.com>
2019-03-28 14:48:13 +0700

    Update RealQuestion2.py

--- Question2/RealQuestion2.py
+++ Question2/RealQuestion2.py
@@ -4,24 +4,32 @@ from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 from A_simple_paint_program import Ui_PaintProgram
 
-class PaintProgram(QMainWindow):
+class PaintProgram(QWidget):
     def __init__(self):
-        QMainWindow.__init__(self,None)
+        QWidget.__init__(self,None)
 
         self.ui = Ui_PaintProgram()
         self.ui.setupUi(self)
+       
+    def mousePressEvent(self, event):
+        print("eee")
+        p = QPainter()
+        p.begin(self)
 
-    
-    def dragEnterEvent(self, event):
-        pencil = QPaint()
-        pencil.begin(self)
+        p.setPen(QColor(0,0,0))
+        p.setBrush(QColor(0,127,0))
+        p.drawPolygon([
+            QPoint(70,100),QPoint(100,110),
+            QPoint(130,100),QPoint(100,150)
+        ])
 
-        pencil.setPen(QColor(0,0,0))
-        pencil.setBrush(QColor(0,0,0))
-
-        pencil.drawEllipse(event.pos().x(),event.pos().y(),2,2)
-
-        pencil.end()
+        p.setPen(QColor(255,127,0))
+        p.setBrush(QColor(255,127,0))
+        p.drawPie(50,150,100,100,0,180*16)
+        p.drawPolygon(
+            [QPoint(50,200),QPoint(150,200),QPoint(100,400),]
+        )
+        p.end()
 
 
 def main():
