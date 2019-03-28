62d87d3e74d3869cda932890770e5194cdc353ee
Sirawap <45166334+Sirawap@users.noreply.github.com>
2019-03-28 15:39:49 +0700

    Create Pole.py

---
+++ Question3/Pole.py
@@ -0,0 +1,25 @@
+from turtle import *
+class Pole():
+    def __init__(self,name,stack,top_position,x_pos,y_pos,thickness,length,color ):
+        self.name = name
+        self.stack = stack
+        self.x = x_pos
+        self.y = y_pos
+        self.thick = thickness
+        self.length = length
+        self.color = color
+        self.stack = []
+        self.top_position = 0
+    def showpole(self):
+        sethead(0)
+        pendown()
+        for i in range (2):
+            fd(20)
+            lt(90)
+            fd(150)
+            lt(90)
+    def pushdisk(self,disk):
+        self.top_position -= 1
+        disk
+    def popdisk(self):
+
